c6bf0f4067482f33309aebcb05ac6ba37a7187a7
Lucas Roseti <lc.roseti@gmail.com>
2025-04-16 11:05:54 -0300

    feat: add after read customers logic

---
+++ srv/main.ts
@@ -0,0 +1,12 @@
+import { Service } from '@sap/cds';
+import { Customers } from '@models/sales';
+
+export default (service: Service) => {
+    service.after('READ', 'Customers', (results: Customers) => {
+        results.forEach(customer => {
+            if (!customer.email.includes('@')) {
+                customer.email = `${customer.email}@email.com`;
+            }
+        });
+    });
+}
