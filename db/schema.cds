e2dafe596fbca686c0f4ebfd2a71db7f95dae63e
Lucas Roseti <lc.roseti@gmail.com>
2025-04-18 13:32:19 -0300

    feat: add products validation

--- db/schema.cds
+++ db/schema.cds
@@ -28,5 +28,6 @@ entity Products {
     key id: UUID;
         name: String(255);
         price: Decimal(15, 2);
+        stock: Integer;
 }
 
--- srv/main.ts
+++ srv/main.ts
@@ -1,5 +1,5 @@
 import cds, { Request, Service } from '@sap/cds';
-import { Customers } from '@models/sales';
+import { Customers, Products, SalesOrderItem, SalesOrderItems } from '@models/sales';
 
 export default (service: Service) => {
     service.after('READ', 'Customers', (results: Customers) => {
@@ -11,6 +11,7 @@ export default (service: Service) => {
     });
     service.before('CREATE', 'SalesOrderHeaders', async (request: Request) => {
         const params = request.data;
+        const items: SalesOrderItems = params.items;
         if (!params.customer_id) {
             return request.reject(400, 'Customer invalid');
         }
@@ -21,6 +22,18 @@ export default (service: Service) => {
         const customer = await cds.run(customerQuery);
         if (!customer) {
             return request.reject(404, 'Customer not found');
+        }
+        const productsIds: string[] = params.items?.map((item: SalesOrderItem) => item.product_id);
+        const productQuery = SELECT.from('sales.Products').where({ id: productsIds });
+        const products: Products = await cds.run(productQuery);
+        for (const item of items) {
+            const dbProduct = products.find(product => product.id === item.product_id);
+            if (!dbProduct) {
+                return request.reject(400, `Product ${item.product_id} not found`);
+            }
+            if (dbProduct.stock === 0) {
+                return request.reject(400, `Product ${dbProduct.name} not in stock`);
+            }
         }
     });
 }
