e4f8124e6ace8e54a518d8a175127a96dba3867c
Lucas Roseti <lc.roseti@gmail.com>
2025-04-23 15:07:08 -0300

    feat: add ProductRepository and ProductRepositoryImpl

---
+++ srv/repositories/product/implementation.ts
@@ -0,0 +1,21 @@
+import cds from '@sap/cds';
+
+import { ProductRepository } from './protocols';
+import { ProductModel, ProductProps } from 'srv/models/product';
+import { Products } from '@models/sales';
+
+export class ProductRepositoryImpl implements ProductRepository {
+    public async findByIds(ids: ProductProps['id'][]): Promise<ProductModel[] | null> {
+        const productQuery = SELECT.from('sales.Products').where({ id: ids });
+        const products: Products = await cds.run(productQuery);
+        if (products.length === 0) {
+            return null;
+        }
+        return products.map(product => ProductModel.with({
+            id: product.id as string,
+            name: product.name as string,
+            price: product.price as number,
+            stock: product.stock as number,
+        }));
+    }
+}
