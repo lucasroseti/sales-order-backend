ccc566eaa0b8de2fbebe4ecdd5eb5c1a60856b55
Lucas Roseti <lc.roseti@gmail.com>
2025-04-23 15:14:53 -0300

    refactor: move before create logic to its own files

--- srv/main.ts
+++ srv/main.ts
@@ -1,6 +1,7 @@
 import cds, { Request, Service } from '@sap/cds';
 import { Customers, Product, Products, SalesOrderHeaders, SalesOrderItem, SalesOrderItems } from '@models/sales';
 import { customerController } from './factories/controllers/customer';
+import { salesOrderHeaderController } from './factories/controllers/sales-order-header';
 import { FullRequestParams } from './protocols';
 
 export default (service: Service) => {
@@ -18,40 +19,11 @@ export default (service: Service) => {
         (request as unknown as FullRequestParams<Customers>).results = customerController.afterRead(customersList);
     });
     service.before('CREATE', 'SalesOrderHeaders', async (request: Request) => {
-        const params = request.data;
-        const items: SalesOrderItems = params.items;
-        if (!params.customer_id) {
-            return request.reject(400, 'Customer invalid');
+        const result = await salesOrderHeaderController.beforeCreate(request.data);
+        if (result.hasError) {
+            return request.reject(400, result.error?.message as string);
         }
-        if (!params.items || params.items?.length === 0) {
-            return request.reject(400, 'Items are required');
-        }
-        const customerQuery = SELECT.one.from('sales.Customers').where({ id: params.customer_id });
-        const customer = await cds.run(customerQuery);
-        if (!customer) {
-            return request.reject(404, 'Customer not found');
-        }
-        const productsIds: string[] = params.items?.map((item: SalesOrderItem) => item.product_id);
-        const productQuery = SELECT.from('sales.Products').where({ id: productsIds });
-        const products: Products = await cds.run(productQuery);
-        for (const item of items) {
-            const dbProduct = products.find(product => product.id === item.product_id);
-            if (!dbProduct) {
-                return request.reject(400, `Product ${item.product_id} not found`);
-            }
-            if (dbProduct.stock === 0) {
-                return request.reject(400, `Product ${dbProduct.name} not in stock`);
-            }
-        }
-        let totalAmount = 0;
-        items.forEach((item) => {
-            totalAmount += (item.price as number) * (item.quantity as number);
-        });
-        if (totalAmount > 30000) {
-            const discount = totalAmount * (10/100);
-            totalAmount = totalAmount - discount;
-        }
-        request.data.totalAmount = totalAmount;
+        request.data.totalAmount = result.totalAmount;
     });
     service.after('CREATE', 'SalesOrderHeaders', async (results: SalesOrderHeaders, request: Request) => {
         const headersAsArray = Array.isArray(results) ? results : [results] as SalesOrderHeaders;
