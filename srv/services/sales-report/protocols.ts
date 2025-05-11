280457af1871a538177d8d39026f5f15c34fceb7
Lucas Roseti <lc.roseti@gmail.com>
2025-05-10 21:11:26 -0300

    feat: adjust in findByDays method parameter

--- srv/services/sales-report/protocols.ts
+++ srv/services/sales-report/protocols.ts
@@ -4,6 +4,6 @@ import { AbstractError } from '@/errors';
 import { ExpectedResult as SalesReportByDays } from '@models/db/types/SalesReportByDays';
 
 export interface SalesReportService {
-    findByDays(days: number): Promise<Either<AbstractError, SalesReportByDays[]>>;
+    findByDays(days?: number): Promise<Either<AbstractError, SalesReportByDays[]>>;
     findByCustomerId(customerId: string): Promise<Either<AbstractError, SalesReportByDays[]>>;
 }
